66256500f0d97b7336085492a5a14fc6502e057b
cgl <123@qq.com>
2022-02-04 19:33:08 +0800

    first commit

---
+++ src/main/java/com/cao/gitTest.java
@@ -0,0 +1,12 @@
+package com.cao;
+
+/**
+ * @Author : CGL
+ * @Date : 2022/2/4 19:32
+ * @Desc :
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
